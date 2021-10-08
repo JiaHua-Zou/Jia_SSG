06a00a37701d55a85ea14dc0f83ca696446e4648
Sirinoks <shinigamoo@gmail.com>
2021-10-07 23:18:24 -0400

    Config file support working

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+const { exit } = require("yargs");
 const readFile = require("./fs-Handler");
 const argv = require("yargs")
   .usage("Usage: node $0 [options] <input>")
@@ -7,18 +8,26 @@ const argv = require("yargs")
   .options({
     i: {
       alias: "input",
-      demandOption: true,
       desc: "Takes in an Input for file/folder",
+      type: "string",
+    },
+    c: {
+      alias: "config",
+      desc: "specifies a file path for a config file",
       type: "string",
     },
     l: {
       alias: "lang",
       desc: "language type used in the HTML",
       type: "string",
-    },
+    }
   })
   .nargs("i", 1)
   .help("h")
   .alias("h", "help").argv;
 
-readFile(argv.input, argv.lang);
+if(!(argv.config||argv.input)){
+  console.log("You have to specify an input path!");
+  exit(1);
+}
+readFile(argv.input, argv.config, argv.lang);
